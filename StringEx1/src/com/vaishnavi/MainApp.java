2a2d20da7221625145cb57177afa444ef44b62de
Admin <Admin@Admin-PC>
2021-06-29 21:04:52 +0530

    completed.

---
+++ StringEx1/src/com/vaishnavi/MainApp.java
@@ -0,0 +1,21 @@
+package com.vaishnavi;
+
+public class MainApp {
+
+	public static void main(String args[])
+	{
+		String username = "Smith674";
+		System.out.println("Username="+username);
+		
+		String su=username.toUpperCase();
+		System.out.println("String in uppercase="+su);
+		
+		String s1=username.toLowerCase();
+		System.out.println("String in Lowercase="+s1);
+		
+		int count=username.length();
+		System.out.println("String Total character="+count);
+		
+	}
+
+}
